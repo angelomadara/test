57af0de2d6fdb8c421884880dd2a706e291a0e36
fsh-kraken <madara.olegna@hotmail.com>
2016-07-13 11:34:45 +0800

    this is another test upload

---
+++ Config/Config.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+* 
+*/
+class Config
+{
+	public function get(){
+		return "Hello this is the config class";
+	}
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+* 
+*/
+class Index
+{
+	public function index(){
+		return "hello world";
+	}
+}
